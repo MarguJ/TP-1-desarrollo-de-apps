c3205e33907098a1e4e48fb951621ec48966c7a5
MarguJ <margulesjoa@gmail.com>
2026-03-13 08:13:04 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Llegar a la esquina
+#(Semaforo en verde (peaton)
+#Autos frenados/bicicletas
+#Obstaculos)
+#Si todo () es falso entonces cruzar
+
+semaforo = input("¿De que color es el semaforo?")
+vehiculosFrenados = bool(input("¿Frenaron los vehiculos? (0=No 1=Si)"))
+obstaculos = bool(input("¿Hay obstaculos en el camino?(0=No 1=Si)"))
